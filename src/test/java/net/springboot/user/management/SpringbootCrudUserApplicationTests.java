c6ec4c7eeb1219f739c0c328f38d3d145a61dd93
MitPatel <pmit9114@gmail.com>
2022-12-20 16:16:14 +0530

    first commit

---
+++ src/test/java/net/springboot/user/management/SpringbootCrudUserApplicationTests.java
@@ -0,0 +1,13 @@
+package net.springboot.user.management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootCrudUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
